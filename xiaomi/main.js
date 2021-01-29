3cf6b305aacf0d68cbd1f1c9ccc41f0cf566a187
Garfield_zhang <929780650@qq.com>
2021-01-29 20:24:57 +0800

    swiper done

---
+++ xiaomi/main.js
@@ -0,0 +1,24 @@
+// swiper
+function startSwiper(){
+  let swiperImgs = document.getElementsByClassName("swiperimg");
+  let swiperIndex = document.getElementsByClassName("swiper-index-wrapper")[0].getElementsByClassName("index")
+  let currentIndex = 0;   // 当前图片
+  swiperImgs[currentIndex].style.opacity = "1"
+  setInterval(()=>{
+    currentIndex++;
+    if(currentIndex >= 5){
+      currentIndex= 0;
+    }
+    for (let i = 0; i < 5; i++){
+      if (i!==currentIndex){
+        swiperImgs[i].style.opacity = "0"
+        swiperIndex[i].classList.remove("active")
+      }else{
+        swiperImgs[i].style.opacity = "1"
+        swiperIndex[i].classList.add("active")
+      }
+    }
+  },3000)
+}
+startSwiper()
+
